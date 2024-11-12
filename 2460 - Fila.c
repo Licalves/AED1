2a18ea0e1f56650d8d96e6032df4b555d039295d
Licalves <luis.alves.20060419@gmail.com>
2024-11-12 16:39:15 -0300

    2460 - Fila.c

--- 2460 - Fila.c
+++ 2460 - Fila.c
@@ -11,12 +11,13 @@ struct fila {
 };
 
 void insere(int n, struct fila *p);
+void buscaRemove(int n, struct fila*p);
+void printaFila(struct fila *p);
 
 int main(void){
 
     int qnt1, qnt2, temp;
     struct fila *p1 = malloc(sizeof(struct fila));
-    struct fila *p2 = malloc(sizeof(struct fila));
 
     //quantidade de pessoas na fila
     scanf("%d", &qnt1);
@@ -24,22 +25,21 @@ int main(void){
     //criando a lista com os indentificadores
     for(int i=0; i<qnt1; i++){
         scanf("%d", &temp);
-        insere(qnt1, p1);
+        insere(temp, p1);
     }
 
     //quantidade de pessoas que sairam da fila
     scanf("%d", &qnt2);
 
-    //criando a lista com as pessoas que sairam da fila
+    //tirando da fila as pessoas que sairam da fila
     for(int i=0; i<qnt2; i++){
-        scanf("%d", temp);
-        insere(qnt2, p2);
+        scanf("%d", &temp);
+        buscaRemove(temp, p1);
     }
 
+    //printando a fila
+    printaFila(p1);
 
-
-
-    
 }
 
 void insere(int n, struct fila *p){
@@ -57,10 +57,30 @@ void insere(int n, struct fila *p){
 
 void buscaRemove(int n, struct fila*p){
     struct fila *a, *b;
-    
+    a = p;
+    b = p->seguinte;
+    while(b != NULL && b->ident != n){
+        a = b;
+        b = b->seguinte;
+    }
+
+    if(b != NULL){
+        a->seguinte = b->seguinte;
+        free(b);
+    }
 
 }
 
-
-
-
+void printaFila(struct fila *p){
+    struct fila *q;
+    int primeiro = 1;
+    for(q = p->seguinte; q != NULL; q = q->seguinte){
+        if(primeiro){
+            printf("%d", q->ident);
+            primeiro = 0;
+        } else {
+            printf(" %d", q->ident);
+        }
+    }
+    printf("\n");
+}
