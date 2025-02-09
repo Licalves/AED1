37cd0946e923643f2287b1cb81867c8f7f016160
Licalves <luis.alves.20060419@gmail.com>
2025-02-09 18:49:13 -0300

    1068 - Balanço de Parênteses 1 .c

--- 1068 - Balanço de Parênteses 1 .c
+++ 1068 - Balanço de Parênteses 1 .c
@@ -5,39 +5,40 @@
 #define MAX 1000
 
 int bemFormada(char s[]);
- 
-int main() {
 
-    char expressao[1000]; int count=0;
+int main() {
+    char expressao[MAX];
     
-    while(count < 10000){
-        fgets(expressao, sizeof(expressao), stdin);
-        expressao[strlen(expressao) -1] = '\0';
-        if(bemFormada(expressao)){
+    while (fgets(expressao, sizeof(expressao), stdin) != NULL) {
+        if (expressao[strlen(expressao) - 1] == '\n') {
+            expressao[strlen(expressao) - 1] = '\0';
+        }
+
+        if (bemFormada(expressao)) {
             printf("correct\n");
         } else {
             printf("incorrect\n");
         }
-        count++;
     }
     
     return 0;
 }
 
-int bemFormada(char s[]){
+int bemFormada(char s[]) {
     char pilha[MAX];
-    int t=0;
-    for(int i=0; s[i] != '\0'; i++){
-        if(s[i] == '('){
-            pilha[t++] = s[i];
-        } else {
-            if(s[i]==')'){
-                if(t==0 || pilha[t-1] != '(' ){
-                    return 0;
-                }
-                t--;
+    int t = 0;
+    
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(') {
+            pilha[t++] = s[i];  
+        } else if (s[i] == ')') {
+            if (t == 0 || pilha[t - 1] != '(') {
+                return 0;  
             }
+            t--;  
         }
     }
+    
     return t == 0;
 }
+
