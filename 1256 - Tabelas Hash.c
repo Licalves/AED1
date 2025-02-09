f91f915070d8c9840a371139edf3443bb5a45174
Licalves <luis.alves.20060419@gmail.com>
2025-02-09 15:22:45 -0300

    1256 - Tabelas Hash.c

--- 1256 - Tabelas Hash.c
+++ 1256 - Tabelas Hash.c
@@ -1,55 +1,39 @@
-//FALTA TERMINAR, PENSAR NOQUE FAZER SOBRE O N 
-
 #include <stdio.h>
 
-typedef struct cel{
-    int chave;
-    struct cel *seg;
-} celula;
+int main(void) {
+    int n, m, c, chaves, keybase;
 
-int main(void){
-    int n, m, c, chaves, keybase, count=0;
-    
     scanf("%d", &n);
-    scanf("%d", &m);
-    scanf("%d", &c);
-    
-    int hashtable[100][10], casa[100];
-    
-    while(count<n){
+
+    for (int count = 0; count < n; count++) { 
+        scanf("%d %d", &m, &c);  
         
-        for(int i=0; i<100; i++){
-        casa[i] = 0;
-            for(int j=0; j<10; j++){
-            hashtable[i][j] = 0;
-            }
+        int hashtable[100][10] = {0}; 
+        int casa[100] = {0}; 
+
+        for (int i = 0; i < c; i++) {
+            scanf("%d", &chaves);
+            keybase = chaves % m;  
+            
+            hashtable[keybase][casa[keybase]] = chaves;
+            casa[keybase]++;
         }
 
-        for(int i=0; i<c; i++){
-            scanf("%d", &chaves);
-            keybase = chaves%13;
-        
-            hashtable[keybase][casa[keybase]] = chaves;
-            casa[keybase] += 1;
-        }
-        
-        int k;
-        for(int i=0; i<m; i++){
-            k=0;
+        for (int i = 0; i < m; i++) {
             printf("%d -> ", i);
-            while(hashtable[i][k] != 0){
+            int k = 0;
+            while (k < casa[i]) {  
                 printf("%d -> ", hashtable[i][k]);
                 k++;
             }
-            printf("\\");
+            printf("\\\n");  
+        }
+
+        if (count < n - 1) { 
             printf("\n");
         }
-        
-        printf("\n");
-        n++;
     }
-    
-    
 
     return 0;
 }
+
