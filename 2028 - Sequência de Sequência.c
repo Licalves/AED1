9d142623895aeae7c64d1d4e52f0ac9b819db842
Licalves <luis.alves.20060419@gmail.com>
2025-02-10 22:02:46 -0300

    2028 - Sequência de Sequência.c

---
+++ 2028 - Sequência de Sequência.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, caso = 1;
+    while (scanf("%d", &n) != EOF) {
+        int tamanho = 1;
+        for (int i = 1; i <= n; i++) {
+            tamanho += i;
+        }
+        
+        printf("Caso %d: %d %s\n", caso++, tamanho, tamanho == 1 ? "numero" : "numeros");
+        
+        printf("0");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                printf(" %d", i);
+            }
+        }
+        printf("\n\n");
+    }
+    return 0;
+}
